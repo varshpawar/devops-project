91aa78af8d73921e2ff2c30dfc46efb9496fd9f2
Varsha Pawar <varshapawar026@gmail.com>
2026-04-27 11:44:35 +0530

    updated app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app=Flask(__name__)
 @app.route('/')
 def home():
-    return "App is running!"
+    return "App is running successfully!"
 @app.route('/users')
 def users():
     return jsonify([
